81ca3aa01a2d0f64525177ba688cebcac99e699c
ivansantiagojr <ivansantiago.junior@gmail.com>
2024-08-25 21:58:18 -0300

    using zig-curl but need to configure a certificate for https

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,13 @@
 const std = @import("std");
 const print = std.io.getStdOut().writer();
-const http = std.http;
-const is_zig_11 = @import("builtin").zig_version.minor == 11;
+const curl = @import("curl");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    var args = try std.process.argsAlloc(allocator);
+    const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
     if (args.len != 2) {
@@ -22,41 +21,25 @@ pub fn main() !void {
     }
     const language = std.mem.sliceTo(args[1], 0);
 
-    var headers = http.Headers{ .allocator = allocator };
-    defer headers.deinit();
-
-    var client = http.Client{ .allocator = allocator };
-    defer client.deinit();
-    var base_url = "https://www.toptal.com/developers/gitignore/api/";
+    const easy = try curl.Easy.init(allocator, .{});
+    defer easy.deinit();
+    const base_url = "https://www.toptal.com/developers/gitignore/api/";
     var url_buffer: [256]u8 = undefined;
     const url_to_request = try std.fmt.bufPrint(&url_buffer, "{s}{s}", .{ base_url, language });
 
-    const uri = try std.Uri.parse(url_to_request);
-    var req = if (is_zig_11) blk: {
-        var req = try client.request(.GET, uri, headers, .{});
-        errdefer req.deinit();
+    const null_term_slice = try allocator.dupeZ(u8, url_to_request);
+    defer allocator.free(null_term_slice);
 
-        try req.start();
-        break :blk req;
-    } else blk: {
-        var req = try client.open(.GET, uri, headers, .{});
-        errdefer req.deinit();
+    std.debug.print("{s}\n", .{null_term_slice});
 
-        try req.send(.{});
-        break :blk req;
-    };
-    defer req.deinit();
+    const resp = try easy.get(null_term_slice);
+    defer resp.deinit();
 
-    try req.wait();
+    // if (resp.status_code != 200) {
+    //     std.debug.print("Probably not a valid language, use: '{s} list' to see the available languages\n", .{args[0]});
+    //     return;
+    // }
 
-    if (req.response.status != .ok) {
-        std.debug.print("Probably not a valid language, use: '{s} list' to see the available languages\n", .{args[0]});
-        return;
-    }
-
-    var rdr = req.reader();
-    const body = try rdr.readAllAlloc(allocator, 1024 * 1024 * 4);
-    defer allocator.free(body);
-
-    try print.print("{s}\n", .{body});
+    try print.print("{d}\n", .{resp.status_code});
+    try print.print("{s}\n", .{resp.body.?.items});
 }
