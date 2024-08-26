25da26e5d2dbce5931d27988fcf28b79deb66983
ivansantiagojr <ivansantiago.junior@gmail.com>
2024-08-25 22:12:25 -0300

    update to zig 0.13

--- src/main.zig
+++ src/main.zig
@@ -1,14 +1,13 @@
 const std = @import("std");
 const print = std.io.getStdOut().writer();
 const http = std.http;
-const is_zig_11 = @import("builtin").zig_version.minor == 11;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const allocator = gpa.allocator();
 
-    var args = try std.process.argsAlloc(allocator);
+    const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
 
     if (args.len != 2) {
@@ -22,31 +21,21 @@ pub fn main() !void {
     }
     const language = std.mem.sliceTo(args[1], 0);
 
-    var headers = http.Headers{ .allocator = allocator };
-    defer headers.deinit();
-
     var client = http.Client{ .allocator = allocator };
     defer client.deinit();
-    var base_url = "https://www.toptal.com/developers/gitignore/api/";
+    const base_url = "https://www.toptal.com/developers/gitignore/api/";
     var url_buffer: [256]u8 = undefined;
     const url_to_request = try std.fmt.bufPrint(&url_buffer, "{s}{s}", .{ base_url, language });
 
     const uri = try std.Uri.parse(url_to_request);
-    var req = if (is_zig_11) blk: {
-        var req = try client.request(.GET, uri, headers, .{});
-        errdefer req.deinit();
+    const buf = try allocator.alloc(u8, 1024 * 1014 * 4);
+    defer allocator.free(buf);
 
-        try req.start();
-        break :blk req;
-    } else blk: {
-        var req = try client.open(.GET, uri, headers, .{});
-        errdefer req.deinit();
-
-        try req.send(.{});
-        break :blk req;
-    };
+    var req = try client.open(.GET, uri, .{ .server_header_buffer = buf });
     defer req.deinit();
 
+    try req.send();
+    try req.finish();
     try req.wait();
 
     if (req.response.status != .ok) {
