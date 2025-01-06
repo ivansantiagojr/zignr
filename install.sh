f47f65c228659c58e5e9273e87b69d77ade5e878
ivansantiagojr <ivansantiago.junior@gmail.com>
2025-01-06 20:49:42 -0300

    feat: create base struct to support MacOS installation

--- install.sh
+++ install.sh
@@ -1,30 +1,62 @@
 #!/bin/bash
 
-binary_url="https://github.com/ivansantiagojr/zignr/releases/download/v0.1.1/binary-0.1.1-x86_64-linux"
-binary_name="zignr"
+APP_VERSION="v0.1.2"
+BASE_URL="https://github.com/brasilisclub/zignr/releases/download/$APP_VERSION"
 
-echo "Downloading zignr..."
-wget -q "$binary_url" -O "$binary_name"
+BINARY_URL_LINUX="$BASE_URL/BINARY-0.1.2-X86_64-LINUX"
+BINARY_URL_MACOS_ARM="$BASE_URL/BINARY-0.1.2-AARCH64-MACOS"
+BINARY_URL_MACOS="$BASE_URL/binary-0.1.2-x86_64-macos"
+APP_NAME="zignr"
+OS_TYPE=$OSTYPE
+CPU_TYPE=$CPUTYPE
 
-if [ $? -ne 0 ]; then
-    echo "Failed to download zignr."
-    exit 1
-fi
 
-echo "Making zignr executable..."
-chmod +x "$binary_name"
+download_and_create_executable() {
+	echo "Downloading zignr..."
+	wget -q "$binary_url" -O "$APP_NAME"
 
-echo "Moving zignr to ~/.local/bin..."
-mv "$binary_name" "$HOME/.local/bin/$binary_name"
+	if [ $? -ne 0 ]; then
+    	echo "Failed to download zignr."
+    	exit 1
+	fi
 
-# Verify installation
-if [ $? -eq 0 ]; then
-    echo "zignr installed successfully!"
-else
-    echo "Failed to move zignr to ~/.local/bin. You might need sudo privileges."
-    exit 1
-fi
+	echo "Making zignr executable..."
+	chmod +x "$APP_NAME"
+}
 
-echo
-echo "add ~/.local/bin to you PATH so you can run zignr from anywhere"
-echo "then use run zignr"
+linux_install () {
+	echo "Moving zignr to ~/.local/bin..."
+	mv "$APP_NAME" "$HOME/.local/bin/$APP_NAME"
+
+	# Verify installation
+	if [ $? -eq 0 ]; then
+	    echo "zignr installed successfully!"
+	else
+	    echo "Failed to move zignr to ~/.local/bin. You might need sudo privileges."
+	    exit 1
+	fi
+
+	echo
+	echo "Add ~/.local/bin to you PATH so you can run zignr from anywhere"
+	echo "Then use run zignr"
+}
+
+macos_install(){
+	echo "Mac installation will be supported soon"
+}
+
+case "$OSTYPE" in
+	linux-gnu)
+		binary_url=$BINARY_URL_LINUX
+		download_and_create_executable
+		linux_install
+		;;
+	darwin)
+		if [[ $CPU_TYPE == "x86_64" ]]; then
+			binary_url=$BINARY_URL_MACOS
+		fi
+		binary_url=$BINARY_URL_MACOS_ARM
+		download_and_create_executable
+		macos_install
+		;;
+esac
